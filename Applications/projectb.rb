016b4c487f4c83fba48e5427eebd320e23e6855a
J. Bedouet <j.bedouet@free.fr>
2014-02-06 00:47:17 +0100

    Read the prefix of another project must be delayed

--- Applications/projectb.rb
+++ Applications/projectb.rb
@@ -6,7 +6,7 @@ class ProjectB < Application
   
   cmake do
     option :BUILD_WITH_A, :BOOLEAN, desc: 'Build with A'
-    option :ProjectA_DIR, :PATH, cond: :BUILD_WITH_A, default: ProjectA.new.prefix.join('share', 'projecta', 'cmake').to_s
+    option :ProjectA_DIR, :PATH, cond: :BUILD_WITH_A, default: lambda { ProjectA.new.prefix.join('share', 'projecta', 'cmake').to_s }
   end
   
   ctest
