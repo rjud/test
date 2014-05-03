8ddfe71d86cf0a67f63b433631555ed3e947a0de
J. Bedouet <j.bedouet@free.fr>
2014-05-03 20:26:52 +0200

    Different forms to set environment

--- Projects/projectc.rb
+++ Projects/projectc.rb
@@ -3,4 +3,14 @@ class ProjectC < Project
   git 'https://github.com/rjud/ProjectC.git'
   cmake
   depend_on ProjectB
+  # Examples to change environment
+  # Universal form
+  binenv << -> { project(:ProjectB).bindir }
+  # Avoid this form since auto-dependency can not detect this case
+  # Simple form with a call to a function
+  binenv << { :ProjectB => :bindir }
+  # Complex form with a call to a function with several arguments
+  libenv << { :ProjectB => [:libdir] }
+  # Complex form with your own code
+  libenv << { :ProjectB => -> prj { prj.libdir } }
 end
