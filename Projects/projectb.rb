65866d0fb9c097e178a782c6469a8f2a17a14692
J. Bedouet <j.bedouet@free.fr>
2014-05-03 20:25:34 +0200

    Remove depend_on

--- Projects/projectb.rb
+++ Projects/projectb.rb
@@ -8,13 +8,14 @@ class ProjectB < Project
   
   cmake do
     option :BUILD_WITH_A, :BOOLEAN, desc: 'Build with A'
-    option :ProjectA_DIR, :PATH, cond: :BUILD_WITH_A, default: -> { project(:ProjectA).prefix.join('share', 'projecta', 'cmake').to_s }
+    option :ProjectA_DIR, :PATH, cond: :BUILD_WITH_A, default: { :ProjectA => -> p { p.prefix.join('share', 'projecta', 'cmake').to_s } }
   end
   
   ctest
   
   redmine 'http://demo.redmine.org', 'projectc'
-  
-  depend_on ProjectA, :BUILD_WITH_A
+    
+  def bindir; prefix.join('bin').to_s; end
+  def libdir; prefix.join('lib').to_s; end
   
 end
